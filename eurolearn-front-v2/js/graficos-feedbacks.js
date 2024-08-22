267fcfa515d6233f6ee8630c88ec1004ccae7d2c
PedroBonelli <128149892+PedroBonelli@users.noreply.github.com>
2024-08-22 11:56:03 -0300

    Add files via upload

--- eurolearn-front-v2/js/graficos-feedbacks.js
+++ eurolearn-front-v2/js/graficos-feedbacks.js
@@ -3,7 +3,7 @@ async function getFeedbackData(filterType){
 
     switch (filterType) {
         case 'month':
-            url = "http://localhost:8080/restfeedbacks/groupbymonths";
+            url = "http://localhost:8080/restfeedbacks/groupbylast12months";
             break;
     
         default:
@@ -16,29 +16,27 @@ async function getFeedbackData(filterType){
 }
 
 function cleanData(data){
-   Object.keys(data).forEach((key) => {
-
-        if (data[key] == NaN || data[key] == null) {
-            data[key] = 0;
-   }
-})
-
+    const cleanDates = [];
+    for(let i = 0; i<data[0].length; i++){
+        cleanDates.push(data[0][i].slice(0, 7).replace("-", "/"));
+    }
+    data[0] = cleanDates;
 }
 
-export default function buildBarGraph(data, htmlCanvasId){
+export default function buildBarGraph(data, htmlCanvasId){  
 
     cleanData(data);
 
     let canvas = document.getElementById(htmlCanvasId);
 
     new Chart(canvas, {
-        type: 'bar',
+        type: 'line',
         data: {
-            labels: Object.keys(data),
+            labels: data[0],
             datasets: [{
-                label: '# of Votes',
-                data: Object.values(data),
-                borderWidth: 1
+                label: 'Índice de satisfação médio',
+                data: data[1],
+                borderWidth: 2
             }]
         },
         options: {
